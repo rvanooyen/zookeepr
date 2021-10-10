4060093e9b1fce2ea7414b751cffe299128cb59f
rvanooyen <robert.vanooyen@gmail.com>
2021-10-10 18:46:28 -0500

    updated front end functionality

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -92,6 +93,18 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) {    
@@ -113,6 +126,10 @@ app.post('/api/animals', (req, res) => {
       res.json(animal);
     }
   });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
