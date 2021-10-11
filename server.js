514f2f802688e29c8254d951cb1df43fbe862c6e
rvanooyen <robert.vanooyen@gmail.com>
2021-10-10 21:29:55 -0500

    fixed refactoring

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
